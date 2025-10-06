e082deff33af49999bc3647dcc3a3d31f2110162
YNotRobots27200 <ynotrobot27200@gmail.com>
2025-10-06 17:38:27 -0500

    add working drive code

--- FtcRobotController-11.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop27200.java
+++ FtcRobotController-11.0/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleop27200.java
@@ -50,15 +50,17 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative OpMode")
-@Disabled
+@TeleOp(name="Basic Teleop 27200", group="Iterative OpMode")
+
 public class BasicTeleop27200 extends OpMode
 {
     // Declare OpMode members.
-
-    private DcMotor leftDrive = null;
-    private DcMotor rightDrive = null;
-
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor intakeMotor = null;
+    private DcMotor frontLeftMotor = null;
+    private DcMotor backLeftMotor = null;
+    private DcMotor frontRightMotor = null;
+    private DcMotor backRightMotor = null;
     /*
      * Code to run ONCE when the driver hits INIT
      */
@@ -69,14 +71,15 @@ public class BasicTeleop27200 extends OpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must correspond to the names assigned during the robot configuration
         // step (using the FTC Robot Controller app on the phone).
-        leftDrive  = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
-
+        frontLeftMotor  = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+        backLeftMotor   = hardwareMap.get(DcMotor.class, "backLeftMotor");
+        frontRightMotor  = hardwareMap.get(DcMotor.class, "frontRightMotor");
+        backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // Pushing the left stick forward MUST make robot go forward. So adjust these two lines based on your first test drive.
         // Note: The settings here assume direct drive on left and right wheels.  Gear Reduction or 90 Deg drives may require direction flips
-        leftDrive.setDirection(DcMotor.Direction.REVERSE);
-        rightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+
 
         // Tell the driver that initialization is complete.
         telemetry.addData("Status", "Initialized");
@@ -94,40 +97,27 @@ public class BasicTeleop27200 extends OpMode
      */
     @Override
     public void start() {
-
+        runtime.reset();
     }
 
     /*
      * Code to run REPEATEDLY after the driver hits START but before they hit STOP
      */
     @Override
-    public void loop() {
-        // Setup a variable for each drive wheel to save power level for telemetry
-        double leftPower;
-        double rightPower;
+    public void loop()
+    {
+        double y = gamepad1.left_stick_y; // Remember, Y stick is reversed!
+        double x = -gamepad1.left_stick_x;
+        double rx = -gamepad1.right_stick_x;
 
-        // Choose to drive using either Tank Mode, or POV Mode
-        // Comment out the method that's not used.  The default below is POV.
+        frontLeftMotor.setPower(y + x + rx);
+        backLeftMotor.setPower(y - x + rx);
+        frontRightMotor.setPower(y - x - rx);
+        backRightMotor.setPower(y + x - rx);
+        if (gamepad1.dpad_down)
+        {
 
-        // POV Mode uses left stick to go forward, and right stick to turn.
-        // - This uses basic math to combine motions and is easier to drive straight.
-        double drive = -gamepad1.left_stick_y;
-        double turn  =  gamepad1.right_stick_x;
-        leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
-        rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
-
-        // Tank Mode uses one stick to control each wheel.
-        // - This requires no math, but it is hard to drive forward slowly and keep straight.
-        // leftPower  = -gamepad1.left_stick_y ;
-        // rightPower = -gamepad1.right_stick_y ;
-
-        // Send calculated power to wheels
-        leftDrive.setPower(leftPower);
-        rightDrive.setPower(rightPower);
-
-        // Show the elapsed game time and wheel power.
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
-        telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        }
     }
 
     /*
